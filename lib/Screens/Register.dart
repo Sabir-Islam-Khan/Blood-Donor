afa5e562f04a4a651d6af7209b02f3997ca24037
Sabir-Islam-Khan <sabir.islam14@gmail.com>
2020-07-16 01:47:47 +0600

    Custom Drawer added

--- lib/Screens/Register.dart
+++ lib/Screens/Register.dart
@@ -1,7 +1,10 @@
 import 'package:blood_donation/Screens/HomePage.dart';
 import 'package:blood_donation/Services/Auth.dart';
+import 'package:blood_donation/Widgets/CustomDrawer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
 
 class RegistrationScreen extends StatefulWidget {
   final Auth auth;
@@ -11,8 +14,19 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String name;
+  void getData() async {
+    User user = await widget.auth.currentUser();
+    String uid = user.uid;
+
+    Future<DocumentReference> snap =
+        Firestore.instance.collection('Users').document(uid);
+  }
+
   @override
   Widget build(BuildContext context) {
+    double totalHeight = MediaQuery.of(context).size.height;
+    double totalWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -32,10 +46,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           leading: GestureDetector(
             onTap: () => Navigator.push(
               context,
-              MaterialPageRoute(
-                builder: (context) => HomePage(
-                  auth: widget.auth,
-                ),
+              PageTransition(
+                type: PageTransitionType.rightToLeft,
+                child: HomePage(auth: widget.auth),
               ),
             ),
             child: Icon(
@@ -54,7 +67,19 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
           ),
         ),
-        endDrawer: Drawer(),
+        body: Container(
+          height: totalHeight * 1,
+          width: totalWidth * 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: totalHeight * 0.03,
+              ),
+              Text(""),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/Widgets/CustomDrawer.dart
@@ -0,0 +1,94 @@
+import 'package:blood_donation/Services/Auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
+import '../Screens/Register.dart';
+
+class CustomDrawer extends StatefulWidget {
+  final Auth auth;
+  CustomDrawer({@required this.auth});
+  @override
+  _CustomDrawerState createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    double totalHeight = MediaQuery.of(context).size.height;
+    double totalWidth = MediaQuery.of(context).size.width;
+    return Drawer(
+      child: SafeArea(
+        child: Container(
+          height: totalHeight * 1,
+          width: totalWidth * 1,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: <Color>[
+                Color.fromRGBO(239, 44, 120, 1),
+                Color.fromRGBO(237, 66, 101, 1),
+                Color.fromRGBO(239, 79, 79, 1),
+              ],
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: totalHeight * 0.02,
+              ),
+              Center(
+                child: Text(
+                  "Menu",
+                  style: GoogleFonts.meriendaOne(
+                    fontSize: 24.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: totalHeight * 0.02,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.leftToRight,
+                      child: RegistrationScreen(auth: widget.auth),
+                    ),
+                  );
+                },
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: totalWidth * 0.03,
+                    ),
+                    Image(
+                      height: 45.0,
+                      width: 45.0,
+                      image: AssetImage('assets/images/donor.png'),
+                    ),
+                    SizedBox(
+                      width: totalWidth * 0.02,
+                    ),
+                    Text(
+                      "Register as a Donor",
+                      style: GoogleFonts.meriendaOne(
+                        fontSize: totalWidth * 0.05,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
